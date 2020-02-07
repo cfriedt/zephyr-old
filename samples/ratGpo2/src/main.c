3d09fc510c13c6d3ce8ceec7582d39b51a99eec8
Christopher Friedt <chrisfriedt@gmail.com>
2020-02-07 10:04:33 -0500

    no signal in this commit

--- samples/ratGpo2/src/main.c
+++ samples/ratGpo2/src/main.c
@@ -22,8 +22,51 @@ PIN_Config pinTable[] =
     PIN_TERMINATE
 };
 
+#define D(fmt, args...) printk( fmt "\n", ##args )
+
+static void describe_rf_event( const RF_EventMask e ) {
+
+	if ( e & RF_EventCmdDone ) D( "A radio operation command in a chain finished.");
+	if ( e & RF_EventLastCmdDone ) D( "The last radio operation command in a chain finished.");
+	if ( e & RF_EventFGCmdDone ) D( "A IEEE-mode radio operation command in a chain finished.");
+	if ( e & RF_EventLastFGCmdDone ) D( "A stand-alone IEEE-mode radio operation command or the last command in a chain finished.");
+	if ( e & RF_EventTxDone ) D( "Packet transmitted");
+	if ( e & RF_EventTXAck ) D( "ACK packet transmitted");
+	if ( e & RF_EventTxCtrl ) D( "Control packet transmitted");
+	if ( e & RF_EventTxCtrlAck ) D( "Acknowledgement received on a transmitted control packet");
+	if ( e & RF_EventTxCtrlAckAck ) D( "Acknowledgement received on a transmitted control packet, and acknowledgement transmitted for that packet");
+	if ( e & RF_EventTxRetrans ) D( "Packet retransmitted");
+	if ( e & RF_EventTxEntryDone ) D( "Tx queue data entry state changed to Finished");
+	if ( e & RF_EventTxBufferChange ) D( "A buffer change is complete");
+	if ( e & RF_EventPaChanged ) D( "The PA was reconfigured on the fly.");
+	if ( e & RF_EventRxOk ) D( "Packet received with CRC OK, payload, and not to be ignored");
+	if ( e & RF_EventRxNOk ) D( "Packet received with CRC error");
+	if ( e & RF_EventRxIgnored ) D( "Packet received with CRC OK, but to be ignored");
+	if ( e & RF_EventRxEmpty ) D( "Packet received with CRC OK, not to be ignored, no payload");
+	if ( e & RF_EventRxCtrl ) D( "Control packet received with CRC OK, not to be ignored");
+	if ( e & RF_EventRxCtrlAck ) D( "Control packet received with CRC OK, not to be ignored, then ACK sent");
+	if ( e & RF_EventRxBufFull ) D( "Packet received that did not fit in the Rx queue");
+	if ( e & RF_EventRxEntryDone ) D( "Rx queue data entry changing state to Finished");
+	if ( e & RF_EventDataWritten ) D( "Data written to partial read Rx buffer");
+	if ( e & RF_EventNDataWritten ) D( "Specified number of bytes written to partial read Rx buffer");
+	if ( e & RF_EventRxAborted ) D( "Packet reception stopped before packet was done");
+	if ( e & RF_EventRxCollisionDetected ) D( "A collision was indicated during packet reception");
+	if ( e & RF_EventModulesUnlocked ) D( "As part of the boot process, the CM0 has opened access to RF core modules and memories");
+	if ( e & RF_EventInternalError ) D( "Internal error observed");
+	if ( e & RF_EventMdmSoft ) D( "Synchronization word detected (MDMSOFT interrupt flag)");
+	if ( e & RF_EventCmdCancelled ) D( "Command canceled before it was started.");
+	if ( e & RF_EventCmdAborted ) D( "Abrupt command termination caused by RF_cancelCmd() or RF_flushCmd().");
+	if ( e & RF_EventCmdStopped ) D( "Graceful command termination caused by RF_cancelCmd() or RF_flushCmd().");
+	if ( e & RF_EventRatCh ) D( "A user-programmable RAT channel triggered an event.");
+	//if ( e & RF_EventPowerUp ) D( "RF power up event. deprecated This event is deprecated. Use RF_ClientEventPowerUpFinished instead.");
+	if ( e & RF_EventError ) D( "Event flag used for error callback functions to indicate an error. See RF_Params::pErrCb.");
+	if ( e & RF_EventCmdPreempted ) D( "Command preempted by another command with higher priority. Applies only to multi-client applications.");
+
+}
+
 void onRatTriggered(RF_Handle h, RF_RatHandle rh, RF_EventMask e, uint32_t compareCaptureTime)
 {
+    //describe_rf_event( e );
     if (e & RF_EventError)
     {
         while(1);
@@ -61,7 +104,7 @@ int main(int argc, char **argv)
         while(1);
     }
 
-    PINCC26XX_setMux(ledPinHandle, IOID_7, PINCC26XX_MUX_RFC_GPO);
+    PINCC26XX_setMux(ledPinHandle, IOID_7, PINCC26XX_MUX_RFC_GPO0);
     rfHandle = RF_open(&rfObject, &RF_prop, (RF_RadioSetup*)&RF_cmdPropRadioDivSetup, &rfParams);
 
     RF_RatConfigOutput_init(&output);
