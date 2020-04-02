10e7c68d42728f4e6e4150662e55b9f7b81af126
Christopher Friedt <chrisfriedt@gmail.com>
2020-04-02 18:18:36 -0400

    stashing

--- samples/subsys/greybus/src/main.c
+++ samples/subsys/greybus/src/main.c
@@ -227,8 +227,8 @@ static const struct gb_transport_backend gb_xport = {
 static void register_gb_platform_drivers() {
 	// I think that this is just platform independent
 #if 0
-	extern const struct gb_gpio_platform_driver gb_gpio_cc13x2r;
-	gb_gpio_register_platform_driver((struct gb_gpio_platform_driver *)&gb_gpio_cc13x2r);
+	extern const struct gb_gpio_platform_driver gb_gpio_sample;
+	gb_gpio_register_platform_driver((struct gb_gpio_platform_driver *)&gb_gpio_sample);
 #else
 	extern const struct gb_gpio_platform_driver gb_gpio_nrf52840;
 	gb_gpio_register_platform_driver((struct gb_gpio_platform_driver *)&gb_gpio_nrf52840);
@@ -407,3 +407,105 @@ start_over:
 		}
 	}
 }
+
+/*
+ * Simple Greybus GPIO abstraction that only uses 1 gpio
+ */
+
+static const unsigned green_led_pin = DT_ALIAS_LED0_GPIOS_PIN;
+static bool green_led_direction = 1; // make it an input by default
+
+static uint8_t gb_gpio_sample_line_count(void) {
+	return 1;
+}
+static int gb_gpio_sample_activate(uint8_t which) {
+	return 0;
+}
+static int gb_gpio_sample_deactivate(uint8_t which) {
+	return 0;
+}
+static int gb_gpio_sample_get_direction(uint8_t which) {
+
+	if (0 != which) {
+		return -ENODEV;
+	}
+
+	return green_led_direction;
+}
+static int gb_gpio_sample_direction_in(uint8_t which) {
+
+	if (0 != which) {
+		return -ENODEV;
+	}
+
+	struct device *green_led_dev;
+	green_led_dev = device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER);
+	gpio_pin_configure(green_led_dev, green_led_pin, GPIO_DIR_IN);
+
+	return 0;
+}
+static int gb_gpio_sample_direction_out(uint8_t which, uint8_t val) {
+
+	if (0 != which) {
+		return -ENODEV;
+	}
+
+	struct device *green_led_dev;
+	green_led_dev = device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER);
+	gpio_pin_configure(green_led_dev, green_led_pin, GPIO_DIR_OUT);
+	gpio_pin_write(green_led_dev, green_led_pin, val);
+
+	return 0;
+}
+static int gb_gpio_sample_get_value(uint8_t which) {
+
+	if (0 != which) {
+		return -ENODEV;
+	}
+
+	struct device *green_led_dev;
+	green_led_dev = device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER);
+	u32_t value;
+	gpio_pin_read(green_led_dev, green_led_pin, &value);
+	return value;
+}
+static int gb_gpio_sample_set_value(uint8_t which, uint8_t val) {
+
+	if (0 != which) {
+		return -ENODEV;
+	}
+
+	struct device *green_led_dev;
+	green_led_dev = device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER);
+	gpio_pin_write(green_led_dev, green_led_pin, val);
+
+	return 0;
+}
+static int gb_gpio_sample_set_debounce(uint8_t which, uint16_t usec) {
+	return -ENOSYS;
+}
+static int gb_gpio_sample_irq_mask(uint8_t which) {
+	return -ENOSYS;
+}
+static int gb_gpio_sample_irq_unmask(uint8_t which) {
+	return -ENOSYS;
+}
+static int gb_gpio_sample_irq_type(uint8_t which, uint8_t type) {
+	return -ENOSYS;
+}
+
+const struct gb_gpio_platform_driver gb_gpio_sample = {
+	.line_count = gb_gpio_sample_line_count,
+	.activate = gb_gpio_sample_activate,
+	.deactivate = gb_gpio_sample_deactivate,
+	.get_direction = gb_gpio_sample_get_direction,
+	.direction_in = gb_gpio_sample_direction_in,
+	.direction_out = gb_gpio_sample_direction_out,
+	.get_value = gb_gpio_sample_get_value,
+	.set_value = gb_gpio_sample_set_value,
+	.set_debounce = gb_gpio_sample_set_debounce,
+	.irq_mask = gb_gpio_sample_irq_mask,
+	.irq_unmask = gb_gpio_sample_irq_unmask,
+	.irq_type = gb_gpio_sample_irq_type,
+};
+
